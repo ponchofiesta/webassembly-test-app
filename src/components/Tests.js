267cdd6cdccd581e5f46e1e7de5f4c515d796bf6
Michael Richter <m.richter@tu-berlin.de>
2019-02-19 17:59:28 +0100

    Async test

--- src/components/Tests.js
+++ src/components/Tests.js
@@ -1,29 +1,36 @@
 import React, { Component } from 'react';
+import Async from 'react-async';
+
+const loadTests = () => fetch('tests.json')
+    .then(response => response.json());
 
 class Tests extends Component {
-
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            data: null,
-        };
-    }
-
-    componentDidMount() {
-        fetch('tests.json')
-            .then(response => response.json())
-            .then(data => this.setState({ data }));
-    }
+    //
+    // constructor(props) {
+    //     super(props);
+    //
+    //     this.state = {
+    //         data: null,
+    //     };
+    // }
 
     render() {
         return <div>
             <h1>Alle Tests</h1>
-            <ul>
-                {
-                    this.data.map(test => <li>{test.name}</li>)
-                }
-            </ul>
+            <Async promiseFn={loadTests}>
+                <Async.Loading>Loading...</Async.Loading>
+                <Async.Resolved>
+                    {data => (
+                        <div>
+                            <strong>Loaded some data:</strong>
+                            <pre>{JSON.stringify(data, null, 2)}</pre>
+                        </div>
+                    )}
+                </Async.Resolved>
+                <Async.Rejected>
+                    {error => `Something went wrong: ${error.message}`}
+                </Async.Rejected>
+            </Async>
         </div>
     }
 }
