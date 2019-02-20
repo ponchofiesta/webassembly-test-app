0949d3d1726f0e15f3d69dd02aa593de14c534ee
Michael Richter <m.richter@tu-berlin.de>
2019-02-20 13:44:53 +0100

    load tests asyncly

--- src/components/Tests.js
+++ src/components/Tests.js
@@ -1,37 +1,43 @@
 import React, { Component } from 'react';
-import Async from 'react-async';
-
-const loadTests = () => fetch('tests.json')
-    .then(response => response.json());
 
 class Tests extends Component {
-    //
-    // constructor(props) {
-    //     super(props);
-    //
-    //     this.state = {
-    //         data: null,
-    //     };
-    // }
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            success: false,
+            error: false,
+            data: null,
+        };
+    }
+
+    async componentDidMount() {
+        try {
+            this.setState({success: false, error: false, data: null});
+            const response = await fetch('tests.json');
+            if (!response.ok) {
+                this.setState({error: response.statusText});
+                return;
+            }
+            const data = await response.json();
+            this.setState({success: true, data});
+        } catch (error) {
+            this.setState({error});
+        }
+    }
 
     render() {
-        return <div>
-            <h1>Alle Tests</h1>
-            <Async promiseFn={loadTests}>
-                <Async.Loading>Loading...</Async.Loading>
-                <Async.Resolved>
-                    {data => (
-                        <div>
-                            <strong>Loaded some data:</strong>
-                            <pre>{JSON.stringify(data, null, 2)}</pre>
-                        </div>
-                    )}
-                </Async.Resolved>
-                <Async.Rejected>
-                    {error => `Something went wrong: ${error.message}`}
-                </Async.Rejected>
-            </Async>
-        </div>
+        if (this.state.error) {
+            return <div>Something went wrong: {this.state.error}</div>
+        } else if (this.state.data) {
+            return <div>
+                <strong>Loaded some data:</strong>
+                <pre>{JSON.stringify(this.state.data, null, 2)}</pre>
+            </div>
+        } else {
+            return <div>Loading...</div>
+        }
     }
 }
 
