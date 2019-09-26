a453c3d01ecb8ce8717dcad0e4d6a245f468cb47
Michael Richter <m.richter@tu-berlin.de>
2019-09-26 10:44:17 +0200

    remove unused import

--- src/components/Benchmark.js
+++ src/components/Benchmark.js
@@ -1,4 +1,4 @@
-import React, {Component, ReactNode} from 'react';
+import React, {Component} from 'react';
 import {Button, Header, Image, List, Message, Segment} from 'semantic-ui-react'
 import config from "../lib/Config";
 
