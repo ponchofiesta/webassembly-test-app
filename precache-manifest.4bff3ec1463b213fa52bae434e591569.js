ccca831f700e696fcf8b4f449a3742218fb916ac
Michael Richter <m.richter@tu-berlin.de>
2019-10-30 14:14:05 +0100

    Updates

--- precache-manifest.4bff3ec1463b213fa52bae434e591569.js
+++ precache-manifest.4bff3ec1463b213fa52bae434e591569.js
@@ -4,7 +4,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/webassembly-test-app/f24b9ab8a89aa4205c15.module.wasm"
   },
   {
-    "revision": "b772fd63864adec97ddf2f66560a0dc8",
+    "revision": "41fac3a2988614dbc970a02852ada49b",
     "url": "/webassembly-test-app/index.html"
   },
   {
@@ -12,7 +12,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/webassembly-test-app/static/css/2.26f5942a.chunk.css"
   },
   {
-    "revision": "d754a16e532eaca7a01d",
+    "revision": "f39f93078827d30c061f",
     "url": "/webassembly-test-app/static/css/main.795991f2.chunk.css"
   },
   {
@@ -24,8 +24,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/webassembly-test-app/static/js/3.53ac83d7.chunk.js"
   },
   {
-    "revision": "d754a16e532eaca7a01d",
-    "url": "/webassembly-test-app/static/js/main.1721600d.chunk.js"
+    "revision": "f39f93078827d30c061f",
+    "url": "/webassembly-test-app/static/js/main.c9fd6fbb.chunk.js"
   },
   {
     "revision": "39e769bd2e872b63b76f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/webassembly-test-app/precache-manifest.8e40e7ac431381951f287faa23962ba9.js"
+  "/webassembly-test-app/precache-manifest.4bff3ec1463b213fa52bae434e591569.js"
 );
 
 self.addEventListener('message', (event) => {
